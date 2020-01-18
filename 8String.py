f54cfcde4b55944bcd1428c258931405a4de64cb
chenboyi081 <chenboyi081@163.com>
2020-01-18 16:59:40 +0800

    学习String

--- 8String.py
+++ 8String.py
@@ -93,3 +93,48 @@ my_value = AssignValue(6)
 print('value 为: {0.value}'.format(my_value))  # "0" 是可选的
 # 数字格式化：	格式具体看：https://www.runoob.com/python/att-string-format.html
 print("{:.2f}".format(3.1415926));
+
+#---------6 Python三引号（triple quotes）
+# python三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符
+# 三引号的语法是一对连续的单引号或者双引号（通常都是成对的用）。
+chat = """how are you?
+i'm fine, than you,and you?
+    me too"""
+print(chat)
+
+#-------------7 Unicode 字符串
+print(u'Hello\u0020World')   #\u0020 标识表示在给定位置插入编码值为 0x0020 的 Unicode 字符（空格符）
+
+#------------8 python的字符串内建函数
+print('a,b'.capitalize())
+print('runoob'.center(20,'*'))  #str.center(width[, fillchar])width -- 字符串的总宽度。fillchar -- 填充字符。
+print('this is string example...wow!!!'.count('wow'))
+print('this is string example...wow!!!'.count('i',4,40));
+import base64
+str = 'this is string example...wow!!!'
+print(str.encode('UTF-8','strict'))
+b64str = base64.b64encode(bytes(str,'utf-8'))
+print(b64str)
+print(base64.b64decode(b64str))
+print(str.endswith('wow!!!'))
+str = 'this is\tstring example...wow!!!'
+print("Original string: " + str)
+print("Defualt exapanded tab: " +  str.expandtabs())    #Python expandtabs() 方法把字符串中的 tab 符号('\t')转为空格，tab 符号('\t')默认的空格数是 8。
+print("Double exapanded tab: " +  str.expandtabs(16))
+str = 'this is string example...wow!!!'
+print(str.find("exam"))
+print(str.find("exam", 10));
+print(str.find("exam", 40));#找不到返回-1
+str2 = 'this2009'    # 字符中没有空格
+print(str2.isalnum())   #Python isalnum() 方法检测字符串是否由字母和数字组成。
+print(str.isalnum())
+print(str2.isalpha())    #如果字符串至少有一个字符并且所有字符都是字母则返回 True，否则返回 False。
+str3 = 'hellomott'
+print(str3.isalpha())
+print(str2.isdecimal())     #如果字符串是否只包含十进制字符返回True，否则返回False。
+str4 = u"2355415"
+print(str4.isdecimal())
+
+
+#另外还有的函数：可以查看https://www.runoob.com/python/python-strings.html
+#TODO:以后用到再做
